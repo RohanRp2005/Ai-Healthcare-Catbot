c17e4fa3e9c9960174658bc79b194ad20fe5e191
Rohan patil <119269254+RohanRp2005@users.noreply.github.com>
2026-06-28 22:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -320,10 +320,9 @@ def chat_msg():
     return jsonify({'status': 'OK', 'answer': response})
 
 
+# Force generation of missing tables upon import (Solves production deployment issues)
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=3000)
-
-
